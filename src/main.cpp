e028ec7d651a42786db6f46498078f3e1d8e7c31
suchusername <prokhoryurij@gmail.com>
2022-05-31 17:27:14 +0000

    refactor main

--- src/main.cpp
+++ src/main.cpp
@@ -18,29 +18,27 @@ int main() {
 
   auto elena_pos = find_offset_between_equal_maps(map_ivan, map_elena);
 
-  if (!elena_pos.has_value()) {
-    std::cout << "Ivan cannot reach Elena." << std::endl;
-  } else {
+  bool can_meet{false};
+  if (elena_pos.has_value()) {
     auto shortest_path =
         map_ivan.find_shortest_path_from_origin(elena_pos.value()).value();
 
-    bool met{false};
     for (size_t idx = 0; 2 * idx <= shortest_path.size(); ++idx) {
       Direction ivan_move = shortest_path[idx];
       Direction elena_move =
           inverse_direction(shortest_path[shortest_path.size() - 1 - idx]);
-      met = fairyland.go(ivan_move, elena_move);
-      if (met) {
+      can_meet = fairyland.go(ivan_move, elena_move);
+      if (can_meet) {
         break;
       }
     }
+  }
 
-    if (met) {
-      std::cout << "Ivan and Elena met in " << fairyland.getTurnCount()
-                << " moves." << std::endl;
-    } else {
-      std::cout << "Ivan still cannot reach Elena." << std::endl;
-    }
+  if (can_meet) {
+    std::cout << "Ivan and Elena met in " << fairyland.getTurnCount()
+              << " moves." << std::endl;
+  } else {
+    std::cout << "Ivan and Elena cannot meet." << std::endl;
   }
 
   write_second_map_onto_first(map_ivan, map_elena, elena_pos);
